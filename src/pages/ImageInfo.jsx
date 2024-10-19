e859d9f45b9f30414dafe50a5e2a9ebebfffd7b5
shohbek <jalilovshohbek9909@gmail.com>
2024-10-19 18:14:35 +0500

    fix

--- src/pages/ImageInfo.jsx
+++ src/pages/ImageInfo.jsx
@@ -10,60 +10,64 @@ function ImageInfo() {
       import.meta.env.VITE_ACCESS_KEY
     }`
   );
-  console.log(data);
 
   return (
-    data && (
-      <div className="my-5 flex justify-center">
-        <div className="flex flex-col md:flex-row gap-12 p-5 shadow-lg">
-          <div className="flex flex-col items-center gap-5">
-            <div className="w-full h-[300px] md:w-[400px] md:h-[300px]">
-              <img
-                className="w-full h-full object-cover rounded-md"
-                src={data.urls.regular}
-                alt={data.alt_description + " avatar"}
-              />
+    <>
+      {isPending && (
+        <span className="loading loading-spinner loading-lg fixed top-1/2 left-1/2"></span>
+      )}
+      {data && (
+        <div className="my-5 flex justify-center">
+          <div className="flex flex-col md:flex-row gap-12 p-5 shadow-lg">
+            <div className="flex flex-col items-center gap-5">
+              <div className="w-full h-[300px] md:w-[400px] md:h-[300px]">
+                <img
+                  className="w-full h-full object-cover rounded-md"
+                  src={data.urls.regular}
+                  alt={data.alt_description + " avatar"}
+                />
+              </div>
+              <div className="flex gap-10">
+                <span className="flex gap-2 items-center">
+                  <FaHeart className="text-2xl" />: <span>{data.likes}</span>
+                </span>
+                <span className="flex gap-2 items-center">
+                  <FaDownload className="text-2xl" />:{" "}
+                  <span>{data.downloads}</span>
+                </span>
+              </div>
             </div>
-            <div className="flex gap-10">
-              <span className="flex gap-2 items-center">
-                <FaHeart className="text-2xl" />: <span>{data.likes}</span>
-              </span>
-              <span className="flex gap-2 items-center">
-                <FaDownload className="text-2xl" />:{" "}
-                <span>{data.downloads}</span>
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div>
-              <strong className="text-lg">Name:</strong>
-              <br />
-              <span className="text-md md:text-xl">{data.user.name}</span>
-            </div>
-            <div>
-              <strong className="text-lg">Location:</strong>
-              <br />
-              <span className="text-md md:text-xl">{data.user.location}</span>
-            </div>
-            <div>
-              <strong className="text-lg">Bio:</strong>
-              <br />
-              <span className="text-md md:text-xl">{data.user.bio}</span>
-            </div>
-            <div>
-              <strong className="text-lg">Portfolio:</strong>
-              <br />
-              <Link
-                to={data.user.portfolio_url}
-                className="text-blue-600 underline text-md md:text-xl"
-              >
-                Portfolio URL
-              </Link>
+            <div className="flex flex-col gap-5">
+              <div>
+                <strong className="text-lg">Name:</strong>
+                <br />
+                <span className="text-md md:text-xl">{data.user.name}</span>
+              </div>
+              <div>
+                <strong className="text-lg">Location:</strong>
+                <br />
+                <span className="text-md md:text-xl">{data.user.location}</span>
+              </div>
+              <div>
+                <strong className="text-lg">Bio:</strong>
+                <br />
+                <span className="text-md md:text-xl">{data.user.bio}</span>
+              </div>
+              <div>
+                <strong className="text-lg">Portfolio:</strong>
+                <br />
+                <Link
+                  to={data.user.portfolio_url}
+                  className="text-blue-600 underline text-md md:text-xl"
+                >
+                  Portfolio URL
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    )
+      )}
+    </>
   );
 }
 
